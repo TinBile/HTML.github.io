78f45044b281fc4e201836d95dd5d1df0ee129f7
TinBile <55217711+TinBile@users.noreply.github.com>
2020-12-19 13:34:32 +0700

    update

---
+++ test1.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+//     console.log("hello");
+//     $('.animate').click(function(){
+//      $("html,body").animate({ scrollTop: $('.service').offset().top} ,1000)
+//     });
+//     $('.animate1').click(function(){
+//         $("html,body").animate({ scrollTop: $('."portfolio').offset().top} ,1000)
+//     });
+//     $('.animate2').click(function(){
+//         $("html,body").animate({ scrollTop: $('.profile').offset().top} ,1000)
+//     });
+    
+// });
+// function createHeart(){
+//     const heart = document.createElement("div");
+
+//     heart.classList.add("heart");
+
+//     heart.style.left = Math.random() * 100 + "vw";
+//     heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+//     heart.innerText = "💛";
+
+//     document.body.appendChild(heart);
+
+//     setTimeout(() => {
+//         heart.remove();
+//     }, 5000);
+// }
+
+// setInterval(createHeart, 300);
+
